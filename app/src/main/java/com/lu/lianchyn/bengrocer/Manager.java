72ed05edf13e2b8dd616af8dd59f8f988d4fac1c
mrayi <ongde-wa15@student.tarc.edu.my>
2018-08-17 17:45:39 +0800

    viewPager

--- app/src/main/java/com/lu/lianchyn/bengrocer/Manager.java
+++ app/src/main/java/com/lu/lianchyn/bengrocer/Manager.java
@@ -1,16 +1,36 @@
 package com.lu.lianchyn.bengrocer;
 
 import android.content.DialogInterface;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager extends AppCompatActivity {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        List<Fragment> listFragments = new ArrayList<Fragment>();
+        listFragments.add(new StaffFragment());
+        listFragments.add(new MemberFragment());
+        listFragments.add(new StockFragment());
+        listFragments.add(new PosFragment());
+
+        MyFragmentPagerAdapter myFragmentPagerAdapter = new MyFragmentPagerAdapter(
+                getSupportFragmentManager(), listFragments);
+        viewPager.setAdapter(myFragmentPagerAdapter);
+
     }
 
     @Override
