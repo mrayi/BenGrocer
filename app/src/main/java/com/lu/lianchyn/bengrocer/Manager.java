9ebc3ec6fa05cc4a70304daf2be4e4f8e28def13
mrayi <ongde-wa15@student.tarc.edu.my>
2018-08-18 10:07:54 +0800

    navigation drawer for inventory staff

--- app/src/main/java/com/lu/lianchyn/bengrocer/Manager.java
+++ app/src/main/java/com/lu/lianchyn/bengrocer/Manager.java
@@ -1,19 +1,58 @@
 package com.lu.lianchyn.bengrocer;
 
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Manager extends AppCompatActivity {
     // Fragment TabHost as mTabHost
     private FragmentTabHost mTabHost;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager);
+       /* setContentView(R.layout.activity_manager);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.profile){
+                    Toast.makeText(Manager.this,"Profile",Toast.LENGTH_SHORT).show();
+                }
+
+
+                else if(id == R.id.aboutus){
+                    Toast.makeText(Manager.this,"About Us",Toast.LENGTH_SHORT).show();
+                }
+                else if(id == R.id.logout){
+                    Toast.makeText(Manager.this,"LogOut",Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+*/
 
         mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
@@ -27,7 +66,12 @@ public class Manager extends AppCompatActivity {
         mTabHost.addTab(mTabHost.newTabSpec("POS").setIndicator("POS"),
                 PosFragment.class, null);
     }
-
+/*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return  abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+    */
     @Override
     public void onBackPressed() {
         new AlertDialog.Builder(this)
